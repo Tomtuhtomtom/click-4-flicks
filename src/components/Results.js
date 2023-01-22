bcd5cfc41d0d1eee186ca1c3340a835d17c2f440
Thomas Morris <tomchrismorris@yahoo.com>
2023-01-21 20:05:14 -0500

    adds colon after Results

--- src/components/Results.js
+++ src/components/Results.js
@@ -6,7 +6,7 @@ export const Results = ({movies}) => {
     if(movies.length > 0){
     return (
         <>
-        <div>Results</div>
+        <div>Results:</div>
         <div>
             {movies.map((movie) => (
                 <div key={movie.id}>
